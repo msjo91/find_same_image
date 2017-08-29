5ded77c25a339ef0e31302ff3d721089edb11246
msjo91 <msjo91@gmail.com>
2017-08-29 15:50:20 +0900

    Complete conveyor.py by modifying explanatory texts, completing methods, etc.

--- module/conveyor.py
+++ module/conveyor.py
@@ -1,100 +1,106 @@
 # -*- coding: utf-8 -*-
+from random import sample, randint
+from tkinter import Label, Canvas
 
-from tkinter import *
-from random import *
-from time import *
+from app import *
 
 
 class Conveyor(Frame):
     def __init__(self, master, picture, width):
-        super(Conveyor, self).__init__()
-        self.image_number_list = []  # 셔플된 이미지의 번호를 저장하기 위한 리스트. 13개
-        self.labels = []  # 컨베이어 frame에 추가되는 이미지 label 위젯의 리스트
-        self.master = master  # 컨베이어 frame의 parent 설정
-        self.width = width  # 메인 테이블의 가로 길이. = 4
-        self.n = width * (width - 1) + 1  # 컨베이어에 넣을 이미지의 수. = 13
-        self.picture = picture  # app에서 생성한 이미지 받아와서 저장
+        super().__init__()
+        self.master = master  # App is parent.
+        self.picture = picture  # App.resized_picture
+        self.width = width  # Width of main table = 4
+        self.n = width * (width - 1) + 1  # Number of images to show on conveyor = 13
+        self.image_number_list = []  # Index list after shuffling App.resized_picture
+        self.labels = []  # Label widget list for conveyor
         self.image_flags = list(
-            False for i in range(self.width * self.width))  # 이미지가 컨베이어에 올라갔는지 아닌지 체크하기 위한 리스트. 초기 세팅은 모두 FALSE.
+            False for i in range(self.width * self.width)
+        )  # Check if image is on conveyor. Default is False.
 
-        self.conveyor_canvas = Canvas(self, height=30)  # 현재 위치 표시를 위한 캔버스 위젯 생성
-
-        # 컨베이어에 올릴 이미지 셔플링
+        # Shuffle self.picture (= App.resized_picture)
         self.random_shuffle()
 
         # TODO
-        # 셔플 결과대로 이미지 label 생성하여 리스트에 저장
-        for i in range(0, self.n):
-            pass
+        # Create image labels.
+        for i in range(self.n):
+            pic_label = Label(self, image=self.picture[self.image_number_list[i]])
+            # Put labels on the conveyor.
+            pic_label.grid(row=1, column=i)
+            # Save labels in the list labels.
+            self.labels.append(pic_label)
+            # Make sure label is on the conveyor.
+            self.image_flags[self.image_number_list[i]] = True
 
-        # 현재 index 설정 = 시작 위치 설정
+        # Set current index = start location
         self.cur_idx = int(self.n / self.width * (self.width - 1))
 
-        # 현재 이미지 설정 = 시작 이미지 설정
-        # 선택한 이미지와 비교 목적으로 저장
+        # Set current image = start image location
+        # Assign to compare with selected image on the main table.
         self.cur_image = self.picture.index(self.picture[self.image_number_list[self.cur_idx]])
 
         # TODO
-        # 캔버스 세팅
+        # Set canvas.
+        # Create canvas widget for index marker.
+        self.canvas_marker = Canvas(self, width=30, height=30)
+        # Create index marker.
+        self.canvas_marker.create_polygon((2, 10), (30, 10), (16, 30), fill='yellow', outline='black')
+        # Grid canvas.
+        self.canvas_marker.grid(row=0, column=9)
+        # Create canvas widget for text Final.
+        self.canvas_text = Canvas(self, width=35, height=20)
+        # Create text.
+        self.text = self.canvas_text.create_text(20, 15, text='FINAL', fill='Red', font='Helvetica 12 bold')
+        # Grid canvas.
+        self.canvas_text.grid(row=0, column=12)
 
     # TODO
-    # 이미지 셔플 함수
     def random_shuffle(self):
-        # 0~15 숫자 중 임의로 중복되지 않는 13개의 숫자 선택
-        pass
+        """Create a list of shuffled indices of self.picture."""
+        self.image_number_list = sample(range(self.width * self.width), self.n)
 
     # TODO
-    # 선택한 그림이 현재 위치의 그림과 일치하는 경우
     def correct_match(self):
-        # 마지막 이미지를 찾은 경우
+        """When selected image and conveyor image are a match"""
+        # When it was the last image
         if self.cur_idx == self.n - 1:
-            pass
-        # 캔버스 위젯
-        # 현재 위치 표시 도형 우측 이동
-        # 현재 이미지 및 현재 위치 재설정
-        # canvas.itemconfig(도형의객체, outline='white', fill='white', + 추가적인 parameter 세팅) 기존에 생성된 도형 객체의 변경 가능
+            self.master.quit_game(win=True)
         else:
-            # 현재 위치가 컨베이어의 가장 우측 도형을 지목할 때
-            # FINAL 글씨를 가리지 않도록 도형 수정
             if self.cur_idx == self.n - 2:
-                pass
-            # 그 외 도형 이동
+                self.cur_idx += 1
+                self.cur_image = self.picture.index(self.picture[self.image_number_list[self.cur_idx]])
+                self.canvas_marker.grid(row=0, column=self.cur_idx)
             else:
-                pass
-                # 현재 이미지 및 현재 위치 수정
+                self.cur_idx += 1
+                self.cur_image = self.picture.index(self.picture[self.image_number_list[self.cur_idx]])
+                self.canvas_marker.grid(row=0, column=self.cur_idx)
 
     # TODO
-    # 선택한 그림이 현재 위치의 그림과 일치하지 않는 경우
     def wrong_match(self):
-        # 마지막 기회에서 틀린 경우
-        if (self.cur_idx == 0):
-            pass
-        # 캔버스 위젯
-        # 가장 왼쪽의 이미지를 제거
-        # 기존 이미지들 좌측으로 한 칸씩 이동
-        # 컨베이어에 추가되지 않은 이미지 중 하나 선택하여 가장 우측에 추가
-        # 현재 위치 재설정
-        # canvas.itemconfig(도형의객체, outline='white', fill='white', + 추가적인 parameter 세팅) 기존에 생성된 도형 객체의 변경 가능
+        """When selected image and conveyor image are not a match"""
+        # When it was the first image
+        if self.cur_idx == 0:
+            self.master.quit_game(win=False)
         else:
-            # FINAL에서 오답 선택했을 때 도형 복구
             if self.cur_idx == self.n - 1:
-                pass
-            # 그 외 도형 이동
+                self.cur_idx -= 1
+                self.cur_image = self.picture.index(self.picture[self.image_number_list[self.cur_idx]])
+                self.canvas_marker.grid(row=0, column=self.cur_idx)
             else:
-                pass
+                self.cur_idx -= 1
+                self.canvas_marker.grid(row=0, column=self.cur_idx)
 
-            # 새 이미지 추가
+            # Get new image
             while True:
                 new_image = randint(0, self.width * self.width - 1)
                 if new_image not in self.image_number_list:
                     break
 
-            # 기존 이미지 좌측으로 한 칸씩 이동
-            # label.config(parameter = configuration) 기존의 label 위젯 변경 가능
+            # Move image to left
             for i in range(0, self.n - 1):
                 self.labels[i].config(image=self.picture[self.image_number_list[i + 1]])
                 self.image_number_list[i] = self.image_number_list[i + 1]
 
-            # 새 이미지 추가
+            # Add new image to conveyor
             self.image_number_list[self.n - 1] = new_image
             self.labels[self.n - 1].config(image=self.picture[self.image_number_list[self.n - 1]])
