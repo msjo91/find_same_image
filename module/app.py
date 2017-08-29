758682ebe91e396b159eb8a04f1ea3226dabbd4a
msjo91 <msjo91@gmail.com>
2017-08-29 11:29:55 +0900

    Modify imports and annotations of app

--- module/app.py
+++ module/app.py
@@ -1,43 +1,51 @@
 # -*- coding: utf-8 -*-
-
-from tkinter import messagebox
+from tkinter import Frame, PhotoImage, messagebox
 from PIL import Image, ImageTk
-from maintable import *
-from conveyor import *
+from conveyor import Conveyor
+from maintable import Maintable
 
 
 class App(Frame):
-    picture_image = []  # 메인 테이블용 도형 이미지
-    alphabet_image = []  # 메인 테이블용 알파벳 이미지
-    resized_picture = []  # 컨베이어용 도형 이미지
+    """Frames and preps."""
+    picture_image = []  # Picture list for main table
+    alphabet_image = []  # Alphabet list for main table
+    resized_picture = []  # Picture list for conveyor
     continue_game = 1
 
     def __init__(self, master, n):
-        super(App, self).__init__()
-        self.master = master  # root를 parent로
+        super().__init__()
+        self.master = master  # root = Tk() is parent.
         self.n = n
-        self.create_images()  # 이미지 읽어와서 저장
+        self.create_images()  # Open, read and write images.
 
-        # Maintable frame widget 생성
+        # Create main table frame widget.
+        # master=App(), picture=App.picture_image, alphabet=App.alphabet_image, width=App.n
         self.table = Maintable(self, self.picture_image, self.alphabet_image, self.n)
+        # Put it on the first row of the frame.
         self.table.grid(row=0, column=0, pady=(10, 20))
 
-        # Conveyor frame widget 생성
+        # Create conveyor frame widget.
+        # master=App(), picture=App.resized_picture, width=App.n
         self.conveyor = Conveyor(self, self.resized_picture, self.n)
+        # Put it on the second row of the frame.
         self.conveyor.grid(row=1, column=0)
 
-    # 이미지 파일을 읽어와서 이미지 객체를 생성하고 리스트에 저장하는 부분
-    # 생성된 이미지 객체 리스트에는 추가적인 변경이 없고, index를 통해 randomize
     def create_images(self):
-        self.picture_image = list(Image.open("picture\\%d.JPG" % (i + 1)) for i in range(self.n * self.n))
-        self.alphabet_image = list(PhotoImage(file="alphabet\\%d.GIF" % (i + 1)) for i in range(self.n * self.n))
+        """
+        Read image files, create image objects and save them in list.
+        Image objects in the list will not be changed.
+        They are only randomized by shuffling indices.
+        """
+        self.picture_image = list(Image.open("../picture/%d.jpg" % (i + 1)) for i in range(self.n * self.n))
+        self.alphabet_image = list(PhotoImage(file="../alphabet/%d.gif" % (i + 1)) for i in range(self.n * self.n))
         self.resized_picture = list(
-            self.picture_image[i].resize((50, 50), Image.ANTIALIAS) for i in range(self.n * self.n))
+            self.picture_image[i].resize((50, 50), Image.ANTIALIAS) for i in range(self.n * self.n)
+        )
         self.resized_picture = list(ImageTk.PhotoImage(self.resized_picture[i]) for i in range(self.n * self.n))
         self.picture_image = list(ImageTk.PhotoImage(self.picture_image[i]) for i in range(self.n * self.n))
 
-    # 게임 종료시 처리 부분
     def quit_game(self, win):
+        """Either win or lose the game; then call continue box."""
         if win is True:
             messagebox.showinfo("게임 종료", "성공하였습니다")
         else:
@@ -47,6 +55,9 @@ class App(Frame):
         if result == 'no':
             App.continue_game = 0
 
+        # Close conveyor.
         self.conveyor.quit()
+        # Close main table.
         self.table.quit()
+        # Close app.
         self.quit()
